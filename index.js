06dc4346ae16fc1deac42aa83cb7dafc66be4dff
gulzar <ali.gulzar@tuni.fi>
2020-11-01 19:22:32 +0500

    Part 3 Saving data to mongoDB exercises done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const express = require("express");
 const logger = require("morgan");
-const cors = require("cors")
+const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 const PORT = process.env.PORT || 3001
-let personData = require("./db.json");
 
 logger.token('bodyData', (req, res) => JSON.stringify(req.body))
 
@@ -18,41 +20,70 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(personData)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const person = personData.find(pd => pd.id === Number(request.params.id))
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  const person = Person.findById(request.params.id)
+  .then(res => response.json(res))
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  const count = personData.length;
-  const date = new Date();
-  response.send(`<p>Phone book has info for ${count} people </p> <p>${date}</p>`)
+  Person.countDocuments().then((number) => {
+    const date = new Date();
+    response.send(`<p>Phone book has info for ${number} people </p> <p>${date}</p>`)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  personData = personData.filter(pd => pd.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id).then(result => {
+    response.status(204).end()
+  }).catch(error => console.log(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const person = request.body;
-  if (personData.find(pd => pd.name === person.name)) {
-    response.json({error: 'name must be unique'}).status(404).end()
-  } else if (!person.name) {
+  if (!person.name) {
     response.json({error: 'Please define person name'}).status(403).end()
   } else if (!person.number) {
     response.json({error: 'Please define person number'}).status(403).end()
   }
-  response.json([...personData, {name: person.name, number: person.number, id: Math.floor(Math.random() * Math.floor(10000000))}])
+  const personDB = new Person({
+    name: person.name,
+    number: person.number
+  })
+  personDB.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
+
+app.put('/api/persons/:id', (request, response) => {
+  const person = request.body
+  const updatePerson = {
+    name: person.name,
+    number: person.number,
+  }
+  Person.findByIdAndUpdate(person.id, updatePerson, { new: true })
+  .then(updatedPerson => response.json(updatedPerson))
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
